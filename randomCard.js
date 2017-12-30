e59ec8d39224f2d00556b4f8c3de14ab23db1248
Leo Bowen-Biggs <leo.bowen.biggs@gmail.com>
2017-12-30 15:49:34 -0800

    navbar contains link to random card (temp/placeholder pages right now)

---
+++ randomCard.js
@@ -0,0 +1,13 @@
+function pickCard() {
+  var i = Math.floor((Math.random() * 4));
+
+  var links = new Array()
+  links[0] = "-0";
+  links[1] = "0";
+  links[2] = "1";
+  links[3] = "2";
+
+  var x = document.getElementById("randomCard");
+  x.href = links[i];
+  console.log(x.href);
+}
