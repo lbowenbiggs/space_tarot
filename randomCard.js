15971a8ba48f1119c917cd34a385cd578d1735a5
Leo Bowen-Biggs <leo.bowen.biggs@gmail.com>
2017-12-30 18:03:16 -0800

    rename '-0.html' so it doesn't start with a special character

--- randomCard.js
+++ randomCard.js
@@ -2,7 +2,7 @@ function pickCard() {
   var i = Math.floor((Math.random() * 4));
 
   var links = new Array()
-  links[0] = "-0";
+  links[0] = "n0";
   links[1] = "0";
   links[2] = "1";
   links[3] = "2";
