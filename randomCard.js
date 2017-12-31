cee632878def1c5f88eb3bf6014235bb78105ca3
Leo Bowen-Biggs <leo.bowen.biggs@gmail.com>
2017-12-30 18:23:37 -0800

    describe 'The Emperess'

--- randomCard.js
+++ randomCard.js
@@ -1,11 +1,12 @@
 function pickCard() {
-  var i = Math.floor((Math.random() * 4));
+  var i = Math.floor((Math.random() * 5));
 
   var links = new Array()
   links[0] = "n0.html";
   links[1] = "0.html";
   links[2] = "1.html";
   links[3] = "2.html";
+  links[4] = "3.html";
 
   var x = document.getElementById("randomCard");
   x.href = links[i];
