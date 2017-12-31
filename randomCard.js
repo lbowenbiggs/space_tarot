f5f8402156ec6bd9817e6ca817cce7cdb4b73593
Leo Bowen-Biggs <leo.bowen.biggs@gmail.com>
2017-12-30 18:06:28 -0800

    add .html extention when randomly picking cards

--- randomCard.js
+++ randomCard.js
@@ -2,10 +2,10 @@ function pickCard() {
   var i = Math.floor((Math.random() * 4));
 
   var links = new Array()
-  links[0] = "n0";
-  links[1] = "0";
-  links[2] = "1";
-  links[3] = "2";
+  links[0] = "n0.html";
+  links[1] = "0.html";
+  links[2] = "1.html";
+  links[3] = "2.html";
 
   var x = document.getElementById("randomCard");
   x.href = links[i];
