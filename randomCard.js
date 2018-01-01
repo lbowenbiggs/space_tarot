5ba83aac96049b4db9f2b875dc8905709ad6cd5b
Leo Bowen-Biggs <leo.bowen.biggs@gmail.com>
2018-01-01 18:58:30 -0500

    describe The Emperor

--- randomCard.js
+++ randomCard.js
@@ -1,5 +1,5 @@
 function pickCard() {
-  var i = Math.floor((Math.random() * 5));
+  var i = Math.floor((Math.random() * 6));
 
   var links = new Array()
   links[0] = "n0.html";
@@ -7,6 +7,7 @@ function pickCard() {
   links[2] = "1.html";
   links[3] = "2.html";
   links[4] = "3.html";
+  links[5] = "4.html";
 
   var x = document.getElementById("randomCard");
   x.href = links[i];
